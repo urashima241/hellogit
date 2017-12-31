6c60015802242ea5e466a900ac3fc3691bdd1271
Satonaka <exot>
2017-12-31 16:35:29 +0900

    最初のコミット

---
+++ tools/src/common/DateUtil.java
@@ -0,0 +1,7 @@
+package common;
+
+public class DateUtil {
+    public static String getDate() {
+        return "2017/12/31";
+    }
+}
---
+++ tools/src/sample/Sample.java
@@ -0,0 +1,11 @@
+package sample;
+
+import common.DateUtil;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        System.out.println(DateUtil.getDate());
+    }
+
+}
